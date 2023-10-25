e5de129ce3ea2fae38ed871e01a585c27fc3e1c4
veyselkayhan <veyselkayhan13@gmail.com>
2023-10-25 09:48:16 +0300

    Initial commit

---
+++ src/main/java/com/veysel/Java11XmlConfiguration/Java11XmlConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.veysel.Java11XmlConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11XmlConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11XmlConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veysel/Java11XmlConfiguration/Java11XmlConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veysel.Java11XmlConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11XmlConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
